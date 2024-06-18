acb2b647f19e9f834abf7feee0220b4c96ced41e
Raelsantana <166087070+Raelsantana@users.noreply.github.com>
2024-06-18 12:21:44 -0300

    Add files via upload
    Algoritmo clássico

---
+++ Teste 1.py
@@ -0,0 +1,13 @@
+#Faça um algoritmo que exiba a mensagem "Olá, mundo!".
+
+#Declarando a variável com a mensagem desejada.
+msg=('Olá, mundo!')
+#Usando a função "print('')" para imprimir na tela a mensagem.
+print(msg)
+
+
+
+
+
+
+
