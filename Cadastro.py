fa849691a7b636ffda6738309f7258278d67d491
Raelsantana <166087070+Raelsantana@users.noreply.github.com>
2024-06-19 17:52:08 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -1,11 +1,11 @@
 #Faça um programa que receba o nome de um usuário e deseje boas vindas.
 
-nome = input('Digite seu nome: ')
-idade = input('Digite sua idade: ')
-peso = input('Digite seu peso: ')
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu peso: '))
 
 print(f'Seja Bem Vindo, {nome}!')
-print(f'Sua idade é {idade}')
-print(f'Você está pesando {peso}Kg')
+print(f'Sua idade é {idade}.')
+print(f'Você está pesando {peso:.1f}Kg.')
 
-
+#Função "input('')" é utilizada para receber dados do usuário.
