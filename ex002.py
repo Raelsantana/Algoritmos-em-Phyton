a9418bc71a77fc80d990cfbd913dd24f225784ad
Raelsantana <166087070+Raelsantana@users.noreply.github.com>
2024-06-19 17:45:27 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,11 @@
+#Faça um programa que receba o nome de um usuário e deseje boas vindas.
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+
+print(f'Seja Bem Vindo, {nome}!')
+print(f'Sua idade é {idade}')
+print(f'Você está pesando {peso}Kg')
+
+
